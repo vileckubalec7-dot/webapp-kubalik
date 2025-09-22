5dd58e3b9bc0d4647199de2b8cafcc7890e0e76d
vileckubalec7-dot <vileckubalec7.com>
2025-09-22 20:14:04 +0200

    feat: add dark mode

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 document.getElementById("btn").addEventListener("click", () => {
   document.getElementById("output").textContent = "Klikol si na tlačidlo! 🎉";
 });
+const body = document.body;
+const toggle = document.createElement("button");
+toggle.textContent = "Prepni tému";
+document.body.appendChild(toggle);
+
+toggle.addEventListener("click", () => {
+  body.classList.toggle("dark");
+});
 
 
